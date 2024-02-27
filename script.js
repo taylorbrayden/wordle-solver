15217d4b9deebf2fcbc1b666bc0aceb6114e79fc
taylorbrayden <159168019+taylorbrayden@users.noreply.github.com>
2024-02-27 20:18:52 +0000

    1:18 02/27/2024

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-/*
 const fs = require('fs')
 
 fs.readFile('five_letter_words.txt', 'utf8', (err, data) => {
     if (err) throw err;
-    
-    const lines = '\"[\"' + data + '\"]\"';
 
-    fs.writeFile('test.json', lines, (err) => {
- 
-        // In case of a error throw err.
-        if (err) throw err;
-    });
-   
+    console.log(data);
 });
-*/
