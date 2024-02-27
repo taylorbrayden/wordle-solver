000665dcb814c7d16364c4288f3fe38ed43e6a3d
taylorbrayden <159168019+taylorbrayden@users.noreply.github.com>
2024-02-27 13:10:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+const fs = require('fs')
+
+fs.readFile('five_letter_words.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    
+    const lines = '\"[\"' + data + '\"]\"';
+
+    fs.writeFile('test.json', lines, (err) => {
+ 
+        // In case of a error throw err.
+        if (err) throw err;
+    });
+   
+});
+*/
